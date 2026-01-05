2cf8bc9da63a3098f43f438401e0492c2f196a98
ChifundoKauwa <bsc-com-15-21@unima.ac.mw>
2026-01-05 06:58:56 +0200

    Fix JWT guard to respect @Public() decorator for auth endpoints

--- invoicing-system/src/auth/guards/jwt.guard.ts
+++ invoicing-system/src/auth/guards/jwt.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorators';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -13,6 +14,16 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
     // Always allow preflight requests
     if (request.method === 'OPTIONS') {
+      return true;
+    }
+
+    // Check if route is marked as public
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
       return true;
     }
 
